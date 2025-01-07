5496321d5e128759f7127c95efb2e9a0b592384c
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-07 13:54:49 -0500

    test commit

--- summary.py
+++ summary.py
@@ -3,15 +3,23 @@ from dotenv import load_dotenv
 from os import getenv
 from transcript import getTextTranscript
 
-load_dotenv() # load env variables located in .env file
-API_KEY = getenv("API_KEY_GEM") # api key stored in .env file
+def config() : 
+    load_dotenv() # load env variables located in .env file
+    API_KEY = getenv("API_KEY_GEM") # api key stored in .env file
+    genai.configure(api_key=API_KEY) 
 
-genai.configure(api_key=API_KEY) 
-model = genai.GenerativeModel("gemini-1.5-flash")
-videoUrl = "https://www.youtube.com/watch?v=J8hzJxb0rpc&pp=ygUOd29ybGQgd2lkZSB3ZWI%3D"
-videoTranscript = getTextTranscript(videoUrl)
-prompt = f"Make a detailed an structured summary of the following youtube video transcript :\n {videoTranscript}"
-response = model.generate_content(prompt)
+def generateSummary(videoUrl : str):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    videoTranscript = getTextTranscript(videoUrl)
+    prompt = f"Use plain text format with bullet points. Make a detailed an structured summary of the following youtube video transcript :\n {videoTranscript}"
+    return model.generate_content(prompt).text
 
-with open('video_summary.txt','w',encoding='utf-8') as file:
-    file.write(response.text)
+config()
+
+DEFAULT_VIDEO_URL = "https://www.youtube.com/watch?v=J8hzJxb0rpc"
+videoUrlInput : str = input("enter the youtube video URL : ")
+videoUrl = videoUrlInput if videoUrlInput else DEFAULT_VIDEO_URL
+summary : str = generateSummary(videoUrl).format()
+
+with open('example-summary.txt','w',encoding='utf-8') as file:
+    file.write(summary)
