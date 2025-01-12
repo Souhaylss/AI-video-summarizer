f5dc1112b4b5abda936d05ab9315c94b5016b838
Souhayl-s <souhaylsejjari1@gmail.com>
2025-01-12 18:15:35 -0500

    Add execution context

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from summary import summarizeVideo
 
 
-summary = summarizeVideo()
-
-with open('summary.html','w',encoding='utf-8') as file:
-    file.write(summary)
+if(__name__ == "__main__"):
+    summary = summarizeVideo()
+    with open('summary.html','w',encoding='utf-8') as file:
+        file.write(summary)
